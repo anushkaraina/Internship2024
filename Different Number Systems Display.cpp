3705377da2c05a4c95e0466a819329fde4908fc2
Anushka Raina <120622673+anushkaraina@users.noreply.github.com>
2024-07-14 19:08:11 +0530

    Create Different Number Systems Display.cpp
    This program demonstrates the representation of the number 15 in different number systems: decimal, octal, hexadecimal, and binary. It displays the values of these representations to illustrate their equivalence.

---
+++ Different Number Systems Display.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n1=15;
+	int n2=017;
+	int n3=0x0F;
+	int n4=0b00001111;
+	cout<<"Number1: " << n1 << endl;
+	cout<<"Number2: " << n2 << endl;
+	cout<<"Number3: " << n3 << endl;
+	cout<<"Number4: " << n4 << endl;
+}

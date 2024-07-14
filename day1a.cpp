ce7a8985028eaa3392a1792337c34451481c4721
Anushka Raina <120622673+anushkaraina@users.noreply.github.com>
2024-07-14 19:04:19 +0530

    Add files via upload

---
+++ day1a.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1, num2;
+	cout << "Enter number1: " ;
+	cin>>num1;
+	cout << "Enter number2: " ;
+	cin>>num2;
+	num1=num1+num2;
+	cout<< "The sum is: " << num1<< endl;
+}

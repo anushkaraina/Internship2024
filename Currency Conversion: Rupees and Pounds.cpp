f6180dfbd129528537e893523c3f942232c648c0
Anushka Raina <120622673+anushkaraina@users.noreply.github.com>
2024-07-14 19:13:53 +0530

    Create Currency Conversion: Rupees and Pounds.cpp
    This program performs basic currency conversion between rupees and pounds. It allows the user to input an amount in rupees and converts it to pounds, and also takes an amount in pounds and converts it to rupees. Note that the conversion rates used in this example are for demonstration purposes and may not reflect current exchange rates.

---
+++ Currency Conversion: Rupees and Pounds.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int r;
+	cout<< "enter rupees: "<< endl;
+	cin>>r;
+	int rp=r*0.0094;
+	cout<<rp<<endl;
+	int p;
+	cout<< "enter pound: "<< endl;
+	cin>>p;
+	int pr=p*106.91;
+	cout<<pr<<endl;
+}
